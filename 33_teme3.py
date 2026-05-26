1726dc2e79a54eaefeeb82f5d8e2962b20a10d37
coribogatan <anacorina.bogatan@gmail.com>
2026-05-26 22:17:47 +0300

    teme fundamentale 3

---
+++ 33_teme3.py
@@ -0,0 +1,28 @@
+var1 = ["ERR-Value Error-ER:10", "INF-Program launch Info-CD:5", "WRN-Low memory-WR:11"]
+
+var2 = ["INF-Program exit-CD:14", "WRN-Low disk space-WR:99", "WRN-Bandwith reached-WR:87"]
+
+# for s in var2:
+#     if s.split("-")[0] == "ERR":
+#         print("[ERROR]")
+#     elif s.split("-")[0] == 'INF':
+#         print("[INFO]")
+#     elif s.split("-")[0] == 'WRN':
+#         print("[WARNING]")
+#     print(f"Mesaj: {s.split('-')[1]}")
+#     # # s -> string :::: .split("-") -> list :::: [2] -> elem din lista dupa index :::: elem este string, deci s.split("-")[2] -> string :::: .split(":") -> list :::: acea_lista[1] -> al doilea element, care este numarul de cod ce ne intereseaza.
+#     print(f"Cod: {s.split("-")[2].split(":")[1]}\n")
+
+def text(var):
+    for s in var:
+        if s.split("-")[0] == "ERR":
+            print("[ERROR]")
+        elif s.split("-")[0] == 'INF':
+            print("[INFO]")
+        elif s.split("-")[0] == 'WRN':
+            print("[WARNING]")
+        print(f"Mesaj: {s.split('-')[1]}")
+        print(f"Cod: {s.split("-")[2].split(":")[1]}\n")
+
+text(var1)
+text(var2)

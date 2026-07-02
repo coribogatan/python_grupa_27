7d86764520dcc8d1a5574218e179775a64f9d9c1
coribogatan <anacorina.bogatan@gmail.com>
2026-07-02 21:01:46 +0300

    implemented tests and observer patterns

---
+++ 21_observer_pattern.py
@@ -0,0 +1,67 @@
+
+class Observer:
+    def update(self, msg):
+        pass
+
+    def finish(self):
+        pass
+
+class EventBus:
+    def __init__(self):
+        self.observers = []
+
+    def register_observer(self, observer: Observer):
+        self.observers.append(observer)
+
+    def unregister_observer(self, observer: Observer):
+        self.observers.remove(observer)
+
+    def notify(self, msg):
+        for obs in self.observers:
+            obs.update(msg)
+
+    def finish_for_all(self):
+        for obs in self.observers:
+            obs.finish()
+
+# Creati o clasa care mosteneste din Observer
+# suprascrieti metoda update, sa printeze acel msg
+# creati o instanta a acestei clase
+# scrieti bus.register_observer() si folositi acea instanta a clasei create mai devreme
+# rulati codul.
+
+
+class Printer(Observer):
+    def update(self, msg):
+        print(msg)
+
+class ToFile(Observer):
+    def __init__(self):
+        self.f = open("logs_01.txt", "w", encoding="utf-8")
+
+    def update(self, msg):
+        self.f.write(msg)
+        self.f.write("\n")
+
+    def finish(self):
+        self.f.close()
+
+
+bus = EventBus()
+print1 = Printer()
+file_writer = ToFile()
+bus.register_observer(print1)
+bus.register_observer(file_writer)
+while True:
+    var1 = input("Message:\n")
+
+    if var1 == "exit":
+        print("Ending program.")
+        break
+
+    bus.notify(var1)
+
+bus.finish_for_all()
+
+
+
---
+++ test_simple_functions.py
@@ -0,0 +1,43 @@
+import pytest
+
+# from simple_functions import * - se importa toate functiile
+from simple_functions import add, is_even, div, to_positive
+
+def test_add():
+    assert add(1, 3) == 4
+    assert add(5, -5) == 0
+    assert add(0, -1) == -1
+    assert add(5, 0.5) == 5.5
+
+def test_is_even():
+    assert is_even(5) == False
+    assert is_even(0) == True
+    assert is_even(-4) == True
+
+def test_div():
+    assert div(5, 2) == 2.5
+    assert div(10, 2) == 5
+    # assert div(3, 0) ==  # ne asteptam ca acest div sa dea exceptia ZeroDivisionError
+    with pytest.raises(ZeroDivisionError):
+        div(3, 0)
+
+def test_to_positive():
+    assert to_positive(-5) == 5
+    assert to_positive(-100.5) == 100.5
+    assert to_positive(4) == 4
+    assert to_positive(0) == 0
+    with pytest.raises(TypeError):
+        to_positive("50")
+
+
+
+
+
+
+
+
+
+
+
+
+
